7c04e6bc5abeb5a2489be8c24aee60222e6e0de5
yodfz <yodfz@qq.com>
2016-10-13 22:56:04 +0800

    项目初始化

---
+++ src/core/directory/animation.js
@@ -0,0 +1,5 @@
+export default {
+    animationName: [
+        'button_ani_1', 'button_ani_2', 'button_ani_3'
+    ]
+}
---
+++ src/core/index.js
@@ -0,0 +1,23 @@
+/**
+ * 动效库
+ */
+import animation from './directory/animation';
+let str = ['ux-animation','_act'];
+export default {
+    install (_opt) {
+        var $hasTouch = 'ontouchstart' in window;
+        let $opt = _opt;
+        document.addEventListener(($hasTouch ? 'touchstart' : 'mousedown'), function (e) {
+            var $that = e.target;
+            // 判断是否处于diasbled状态
+            if ($that.disabled) return;
+            // 查询是否设置了动效 并且判断状态不处于动效状态中
+            if ($that.dataset[str[0]] && $that.className.indexOf(str[0] + str[1]) === -1) {
+                $that.classList.add($that.dataset.animation + str[1]);
+                setTimeout(function () {
+                    $that.classList.remove($that.dataset.animation + str[1]);
+                }, $opt.timeout);
+            }
+        });
+    }
+};
