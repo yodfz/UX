4a021c3aba61784b7a4fe170150628e93ca45153
yodfz <yodfz@qq.com>
2016-10-14 16:30:04 +0800

    update

--- src/core/index.js
+++ src/core/index.js
@@ -2,7 +2,7 @@
  * 动效库
  */
 import animation from './directory/animation';
-let str = ['ux-animation','_act'];
+let str = ['ux-animation', '_act'];
 export default {
     install (_opt) {
         var $hasTouch = 'ontouchstart' in window;
@@ -12,11 +12,13 @@ export default {
             // 判断是否处于diasbled状态
             if ($that.disabled) return;
             // 查询是否设置了动效 并且判断状态不处于动效状态中
+
+            // 尝试读取用户是否配置了timeout
             if ($that.dataset[str[0]] && $that.className.indexOf(str[0] + str[1]) === -1) {
                 $that.classList.add($that.dataset.animation + str[1]);
                 setTimeout(function () {
                     $that.classList.remove($that.dataset.animation + str[1]);
-                }, $opt.timeout);
+                }, $opt.timeout || 100);
             }
         });
     }
